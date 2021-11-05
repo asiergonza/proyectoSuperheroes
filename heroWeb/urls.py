de3ab6515609ff769f80acbb4f4f8b595c24cd74
Javier Lazaro <javier.lazaro@opendeusto.es>
2021-11-05 12:57:37 +0100

    Comenzada configuracion urls.py

--- heroWeb/urls.py
+++ heroWeb/urls.py
@@ -7,7 +7,15 @@ from . import views
 
 urlpatterns = [
     
-    # ej: /miApp/
+    # ej: /heroApp/
     path('', views.index, name='index'),
+
+    #/heroApp/grupos
+    #path('grupos', noseque.as_view(), name='grupos'),
+    #/heroApp/grupos/[id_Superheroe] --> Por si hay dos superheroes con nombres iguales
+    #path('grupos/<int:pk>', noseque2.as_view(), name='superheroes'),
+    #/heroApp/superehores/[nombre_Habilidad] 
+    #path('superheroes/<str:nombre_superpoder>', noseque3.as_view(), name='superpoderes')
+
 ]
 
