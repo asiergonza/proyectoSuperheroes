88eb2c908b9e3ee94968e00583862f734a34eb22
Javier Lazaro <javier.lazaro@opendeusto.es>
2021-11-05 13:16:27 +0100

    Creadas las primeras vistas basadas en clases

--- heroWeb/urls.py
+++ heroWeb/urls.py
@@ -1,5 +1,6 @@
 from django.http import request
 from django.urls import path
+from .views import GrupoDetailView
 
 
 from . import views
@@ -7,15 +8,15 @@ from . import views
 
 urlpatterns = [
     
-    # ej: /heroApp/
+    # ej: /heroWeb/
     path('', views.index, name='index'),
 
-    #/heroApp/grupos
-    #path('grupos', noseque.as_view(), name='grupos'),
+    #/heroWeb/grupos
+    path('<str:nombre>/', GrupoDetailView.as_view(), name='grupos'),
     #/heroApp/grupos/[id_Superheroe] --> Por si hay dos superheroes con nombres iguales
-    #path('grupos/<int:pk>', noseque2.as_view(), name='superheroes'),
+    #path('grupos/<int:pk>', SupereroesDetailView.as_view(), name='superheroes'),
     #/heroApp/superehores/[nombre_Habilidad] 
-    #path('superheroes/<str:nombre_superpoder>', noseque3.as_view(), name='superpoderes')
+    #path('superheroes/<str:nombre_superpoder>', SuperpoderesDetailView.as_view(), name='superpoderes')
 
 ]
 
--- heroWeb/views.py
+++ heroWeb/views.py
@@ -1,6 +1,12 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Grupo, Superheroe, Superpoder 
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 # Create your views here.
 def index(request):
     return HttpResponse('Página inicial')
+
+class GrupoDetailView(DetailView):
+    model = Grupo
