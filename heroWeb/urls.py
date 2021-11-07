1965a8620f102a590e47f8015947f8635baccc5f
TheMrguiller <guvillat@opendeusto.es>
2021-11-07 16:28:40 +0100

    Parte editorial y grupo
    Terminada esta parte

--- heroWeb/urls.py
+++ heroWeb/urls.py
@@ -13,10 +13,10 @@ urlpatterns = [
     # ej: /heroWeb/
     path('', views.index, name='index'),
     path('grupos/', GrupoListView.as_view(), name='grupos'), #lista de grupos
-    path('grupos/<str:nombre>/', GrupoDetailView.as_view(), name='grupo'), #Grupo especifico
+    path('grupos/<int:pk>/', GrupoDetailView.as_view(), name='grupo'), #Grupo especifico
     path('superheroes/', SuperheroesListView.as_view(), name='superheroes'),#Lista de superheroes
-    path('superheroes/<int:id>/', SuperheroesDetailView.as_view(), name='superheroe'), #Un heroe especifico
+    path('superheroes/<int:pk>/', SuperheroesDetailView.as_view(), name='superheroe'), #Un heroe especifico
     path('superpoderes/', SuperpoderListView.as_view(), name='superpoderes'), #Lista superpoderes
-    path('superpoderes/<int:id>/', SuperpoderDetailView.as_view(), name='superpodere'), #Un superpoder especifico
+    path('superpoderes/<int:pk>/', SuperpoderDetailView.as_view(), name='superpodere'), #Un superpoder especifico
 ]
 
--- heroWeb/views.py
+++ heroWeb/views.py
@@ -15,19 +15,40 @@ def index(request):
 
 class GrupoDetailView(DetailView):
     model = Grupo
+    context_object_name= 'grupoespecifico'
+    template_name= 'grupo.html'
+
+    def get_context_data(self, **kwargs):
+        # Cargar el contexto base
+        context = super().get_context_data(**kwargs)
+        # Añadir un listado de departamentos
+        context['editorial'] = Grupo.objects.all()
+        return context
+
 class GrupoListView(ListView):
     model = Grupo
     queryset = Grupo.objects.all()
+    context_object_name= 'editorial'
     template_name= 'grupo_list.html'
+
 class SuperheroesDetailView(DetailView):
     model = Superheroe
+
 class SuperheroesListView(ListView):
     model = Superheroe
     queryset = Superheroe.objects.all()
     context_object_name = 'superheroe'
 	
+    def get_context_data(self, **kwargs):
+        # Cargar el contexto base
+        context = super().get_context_data(**kwargs)
+        # Añadir un listado de departamentos
+        context['editorial'] = Grupo.objects.all()
+        return context
+        
 class SuperpoderDetailView(DetailView):
     model = Superpoder
+
 class SuperpoderListView(ListView):
     model = Superpoder
     queryset = Superpoder.objects.all()
