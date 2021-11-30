81193c6c1bf42acb85bc6c4eaa568dcc4768da28
Asier Gonzalez <asier.gonza@opendeusto.es>
2021-11-30 09:33:09 +0100

    cambio model y nuevos personajes
    en clase. :)

--- heroWeb/models.py
+++ heroWeb/models.py
@@ -6,7 +6,7 @@ class Grupo(models.Model):
     nombre = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=350)
     icono = models.URLField(max_length=300)
-    marca = models.TextChoices('Marca', 'DC MARVEL OTROS') 
+    marca = models.TextChoices('Marca', 'DC MARVEL OTROS')
     def __str__(self) -> str:
         return f"{self.nombre}"
 
@@ -17,6 +17,7 @@ class Superheroe(models.Model):
     icono = models.URLField(max_length=300)
     fecha_nacimiento = models.DateField()
     muerto = models.BooleanField()
+    id_api = models.CharField(max_length=20, blank=True)
     def __str__(self) -> str:
         return f"{self.nombre}"
 
